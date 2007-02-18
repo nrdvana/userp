22a12a81a088c126e40f477e8c7d99d77dd03f9d
silverdirk <silverdirk@conserv.silverdirk.com>
2007-02-18 00:55:19 -0400

    Added CommonTypes test cases for types TInt, TBool, TFloat{32,64} and TStrUTF{8,16}

--- src/com/silverdirk/userp/_TestCommonTypes.java
+++ src/com/silverdirk/userp/_TestCommonTypes.java
@@ -17,14 +17,17 @@ public class _TestCommonTypes extends TestCase {
 		UserpType t;
 		int bits;
 		BigInteger range;
+		boolean hasScalar, isScalar;
 
-		TypeAndProps(UserpType rt, int bits, long range) {
-			this(rt, bits, BigInteger.valueOf(range));
+		TypeAndProps(UserpType rt, int bits, long range, boolean hasScalar, boolean isScalar) {
+			this(rt, bits, BigInteger.valueOf(range), hasScalar, isScalar);
 		}
-		TypeAndProps(UserpType rt, int bits, BigInteger range) {
+		TypeAndProps(UserpType rt, int bits, BigInteger range, boolean hasScalar, boolean isScalar) {
 			this.t= rt;
 			this.bits= bits;
 			this.range= range;
+			this.hasScalar= hasScalar;
+			this.isScalar= isScalar;
 		}
 	}
 
@@ -35,16 +38,30 @@ public class _TestCommonTypes extends TestCase {
 	protected void setUp() throws Exception {
 		super.setUp();
 		types= new TypeAndProps[] {
-			new TypeAndProps(CommonTypes.TInt8, 8, 1L<<8),
-			new TypeAndProps(CommonTypes.TInt16, 16, 1L<<16),
-			new TypeAndProps(CommonTypes.TInt32, 32, 1L<<32),
-			new TypeAndProps(CommonTypes.TInt64, 64, BigInteger.ONE.shiftLeft(64)),
-			new TypeAndProps(CommonTypes.TByte, 8, 1L<<8),
-			new TypeAndProps(CommonTypes.TInt16u, 16, 1L<<16),
-			new TypeAndProps(CommonTypes.TInt32u, 32, 1L<<32),
-			new TypeAndProps(CommonTypes.TInt64u, 64, BigInteger.ONE.shiftLeft(64)),
+			new TypeAndProps(CommonTypes.TBool, 1, 2, true, true),
+
+			new TypeAndProps(CommonTypes.TInt8, 8, 1L<<8, true, true),
+			new TypeAndProps(CommonTypes.TInt16, 16, 1L<<16, true, true),
+			new TypeAndProps(CommonTypes.TInt32, 32, 1L<<32, true, true),
+			new TypeAndProps(CommonTypes.TInt64, 64, BigInteger.ONE.shiftLeft(64), true, true),
+
+			new TypeAndProps(CommonTypes.TByte, 8, 1L<<8, true, true),
+			new TypeAndProps(CommonTypes.TInt16u, 16, 1L<<16, true, true),
+			new TypeAndProps(CommonTypes.TInt32u, 32, 1L<<32, true, true),
+			new TypeAndProps(CommonTypes.TInt64u, 64, BigInteger.ONE.shiftLeft(64), true, true),
+
+			new TypeAndProps(CommonTypes.TInt, -1, -1, true, false),
+
+			new TypeAndProps(CommonTypes.TFloat32, 1, 2, true, false),
+			new TypeAndProps(CommonTypes.TFloat64, 1, 2, true, false),
+
+			new TypeAndProps(CommonTypes.TByteArray, -1, -1, true, false),
+			new TypeAndProps(CommonTypes.TStrUTF8, -1, -1, true, false),
+			new TypeAndProps(CommonTypes.TStrUTF16, -1, -1, true, false),
 		};
 		values= new Object[][] {
+			new Object[] { Boolean.TRUE, Boolean.FALSE },
+
 			new Object[] { new Byte((byte)0), new Byte((byte)127), new Byte((byte)-128), },
 			new Object[] { new Short((short)0), new Short((short)0x7FFF), new Short((short)0x8000), },
 			new Object[] { new Integer(0), new Integer(0x7FFFFFFF), new Integer(-0x80000000), },
@@ -54,8 +71,20 @@ public class _TestCommonTypes extends TestCase {
 			new Object[] { new Short((short)0), new Short((short)0x7FFF), new Short((short)0x8000), },
 			new Object[] { new Integer(0), new Integer(0x7FFFFFFF), new Integer(-0x80000000), },
 			new Object[] { new Long(0), new Long(0x7FFFFFFFFFFFFFFFL), new Long(0xFFFFFFFFFFFFFFFFL), },
+
+			new Object[] { BigInteger.valueOf(0x1234), BigInteger.valueOf(-0x1234) },
+
+			new Object[] { new Float(0), new Float(1.625) },
+			new Object[] { new Double(0), new Double(1.625) },
+
+			new Object[] { bytesFromHex("01 02 03 04 05 7F 80 81 99 FF") },
+
+			new Object[] { "Foo", "", "\u8B58" /* Kanji, chosen at random from charmap.*/ },
+			new Object[] { "Foo", "", "\u8B58" },
 		};
 		encodings= new byte[][] {
+			bytesFromHex("01 00"),
+
 			bytesFromHex("00 7F 80"),
 			bytesFromHex("0000 7FFF 8000"),
 			bytesFromHex("00000000 7FFFFFFF 80000000"),
@@ -65,6 +94,16 @@ public class _TestCommonTypes extends TestCase {
 			bytesFromHex("0000 7FFF 8000"),
 			bytesFromHex("00000000 7FFFFFFF 80000000"),
 			bytesFromHex("0000000000000000 7FFFFFFFFFFFFFFF FFFFFFFFFFFFFFFF"),
+
+			bytesFromHex("9234 009234"),
+
+			bytesFromHex("00000000 3FD00000"), // 0 01111111 10100000000000000000000
+			bytesFromHex("0000000000000000 3FFA000000000000"), // 0 01111111111 1010000000000000000000000000000000000000000000000000
+
+			bytesFromHex("0A 01 02 03 04 05 7F 80 81 99 FF"),
+
+			bytesFromHex("03466F6F  00  03E8AD98"),
+			bytesFromHex("030046006F006F  00  018B58"),
 		};
 	}
 
@@ -78,8 +117,8 @@ public class _TestCommonTypes extends TestCase {
 	public void testTypeProperties() {
 		for (int i=0; i<types.length; i++) {
 			String msg= "for type "+types[i].t.getName();
-			assertTrue(msg, types[i].t.hasScalarComponent());
-			assertTrue(msg, types[i].t.isScalar());
+			assertEquals(msg, types[i].hasScalar, types[i].t.hasScalarComponent());
+			assertEquals(msg, types[i].isScalar, types[i].t.isScalar());
 			assertEquals(msg, types[i].bits, types[i].t.getScalarBitLen());
 			assertEquals(msg, types[i].range, types[i].t.getScalarRange());
 			UserpType aClone= types[i].t.makeSynonym(types[i].t.getMeta());
@@ -99,7 +138,11 @@ public class _TestCommonTypes extends TestCase {
 			UserpReader reader= new UserpReader(bseq, bseq.getStream(0), UserpFileReader.rootTypes);
 			for (int j=0; j<values[i].length; j++) {
 				reader.startFreshElement(types[i].t);
-				assertEquals(msg, values[i][j], reader.readValue());
+				Object val= reader.readValue();
+				if (val.getClass().isArray())
+					assertTrue(Util.arrayEquals(true, values[i][j], val));
+				else
+					assertEquals(msg, values[i][j], val);
 			}
 		}
 	}
@@ -128,6 +171,6 @@ public class _TestCommonTypes extends TestCase {
 			ch1+= 9;
 		if (ch2 > 0x40)
 			ch2+= 9;
-		return (byte) (((ch1<<4)&0xF0) | (ch2&0x0F));
+		return (byte) (((ch1&0x0F)<<4) | (ch2&0x0F));
 	}
 }
