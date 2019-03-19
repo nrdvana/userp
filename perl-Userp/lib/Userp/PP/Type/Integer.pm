d3f4b120727b38c980f33c839aba86006885d90b
Michael Conrad <mike@nrdvana.net>
2019-03-19 19:16:53 -0400

    Rework the Integer type to be more directly useful
    It was possible to define 2's complement as a union of two finite
    integer ranges, but that adds messy details to the decoding.  This
    way the standard int types can just be basic configurations.

--- perl-Userp/lib/Userp/PP/Type/Integer.pm
+++ perl-Userp/lib/Userp/PP/Type/Integer.pm
@@ -2,15 +2,40 @@ package Userp::PP::Type::Integer;
 use Moo;
 with 'Userp::PP::Type';
 
-has min_val => ( is => 'ro' );
-has max_val => ( is => 'ro' );
+=head1 DESCRIPTION
+
+The Integer type can represent an infinite range of integers, a half-infinite range, or a
+finite range.  As a special case, it can be encoded in 2's complement.
+
+An Integer type can also have named values.  When decoded, the value will be both an integer
+and a symbol.  This allows for the typical style of C enumeration, but to make a pure
+enumeration of symbols, just add lots of Identifier values to a Choice type.
+
+=head1 ATTRIBUTES
+
+=head2 min_val
+
+The lower end of the integer range, or undef to extend infinitely in the negative direction.
+
+=head2 max_val
+
+The upper end of the integer range, or undef to extend infinitely in the positive direction.
+
+=head2 twos_complement
+
+If nonzero, this is the number of bits in which the integer will be encoded as 2's complement.
+
+=head2 aliases
+
+A hashref of C<< { int => Identifier } >>, optional.
+
+=cut
+
+has min_val         => ( is => 'ro' );
+has max_val         => ( is => 'ro' );
+has twos_complement => ( is => 'ro' );
+has aliases         => ( is => 'ro' );
 
 sub isa_int { 1 }
-
-sub _build_discrete_val_count {
-	my $self= shift;
-	return undef unless defined $self->min_val && defined $self->max_val;
-	return $self->max_val - $self->min_val + 1;
-}
 
 1;
