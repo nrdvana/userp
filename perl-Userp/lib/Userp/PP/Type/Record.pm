f3fba82cb85ceadc42218b83df0611eb1b738ce0
Michael Conrad <mike@nrdvana.net>
2019-03-18 20:48:39 -0400

    Divide sequence into array and record
    This allows arrays to gain a dimension feature and ability to declare
    arrays without pre-defining the type without also cluttering the code
    related to records.

---
+++ perl-Userp/lib/Userp/PP/Type/Record.pm
@@ -0,0 +1,100 @@
+package Userp::PP::Type::Record;
+use Moo;
+with 'Userp::PP::Type';
+
+=head1 DESCRIPTION
+
+A record is a sequence where the elements can each have their own delcared type, and each
+element can be named with an identifier.  Records can be encoded as a fixed count of all its
+elements, as a variable number of defined elements out of an available set, as a sequence of
+ad-hoc key/value pairs, or as some mix of those.  In each case, the element names *must* be
+unique (or undefined), and the application should not depend on the order of elements.
+
+The Record type serves a dual role as a way to match encoding with low-level records like
+C structs, or to store sparsely-encoded key/value pairs for larger records that are not
+practical to encode or even declare in their entirety.   Records are not a generic key/value
+dictionary though; identifiers must adhere to a restricted set of Unicode and cannot be generic
+data.  (for that, just declare a Sequence of type (key,value), and handle the conversion to
+a dictionary within the applicaion)
+
+=head1 ATTRIBUTES
+
+=head2 fields
+
+An arrayref of C<< [Identifier, Type] >>.  Identifier may be C<undef> in which case the field
+is understood to be "filler" or "reserved".  Values will be automatically encoded as zero, and
+skipped over during decoding.
+
+The order of this list matters for the encoding of the first N C<static_fields>.
+
+=head2 static_fields
+
+A count of how many elements of the C<fields> array will be written into every record.
+Any field above this number will be available as a dynamic field.
+
+=head2 other_fields
+
+The type to use for other ad-hoc fields.  Set to C<undef> to disallow ad-hoc fields.
+
+=cut
+
+has fields        => ( is => 'ro', required => 1 );
+has static_fields => ( is => 'ro' );
+has other_fields  => ( is => 'ro' );
+
+sub isa_rec { 1 }
+
+sub has_scalar_component { !defined shift->len_const }
+
+has sizeof    => ( is => 'lazy', builder => sub { shift->_calc_size } );
+has bitsizeof => ( is => 'lazy', builder => sub { shift->sizeof << 3 } );
+
+sub _build_sizeof {
+	my ($self)= @_;
+	# Need constant length
+	my $len= $self->const_len;
+	return undef unless defined $len;
+	return 0 if !$len;
+	my $spec= $self->elem_spec;
+	if (ref $spec eq 'ARRAY') {
+		return undef unless @$spec >= $len;
+		my $bitsize= 0;
+		for (0 .. $len-1) {
+			my $type= $spec->[$_]{type};
+			# each element must also be constant-width
+			my $type_bits= $type->bitsizeof;
+			defined $type_bits or return undef;
+			# bit and byte alignment can be predicted; multibyte alignment can't
+			my $align= $spec->[$_]{align} || 0;
+			if ($align > 0) {
+				return undef; # can't predict length
+			} elsif ($align < 0) {
+				# This field is bit-aligned.
+				$bitsize += $type->bitsizeof;
+			}
+			else {
+				# Byte aligned.  If prev was bit-aligned, round up
+				$bitsize= ($bitsize + 7) & ~7 if $bitsize & 7;
+				$bitsize += $type->sizeof << 3;
+			}
+		}
+		return $bitsize;
+	}
+	else {
+		my $type= $spec->{type};
+		my $type_bits= $type->bitsizeof;
+		defined $type_bits or return undef;
+		my $align= $spec->{align} || 0;
+		if ($align > 0) {
+			return undef;
+		}
+		elsif ($align < 0) {
+			return (($type_bits * $len) + 7) & ~7;
+		}
+		else {
+			return (($type_bits + 7) & ~7) * $len;
+		}
+	}
+}
+
+1;
--- perl-Userp/lib/Userp/PP/Type/Sequence.pm
+++ perl-Userp/lib/Userp/PP/Type/Sequence.pm
@@ -2,139 +2,38 @@ package Userp::PP::Type::Sequence;
 use Moo;
 with 'Userp::PP::Type';
 
+=head1 DESCRIPTION
+
+A sequence is composed of a list of elements identified only by their position in the list.
+The elements of the list might be logically arranged as a single or multi-dimension array.
+The elements of the list share a common type, though that type may be a Choice of other types.
+The length of the sequence may be defined as part of the type, or left unspecified to be
+decoded from the data of each instance.
+
 =head1 ATTRIBUTES
 
-=head2 len
+=head2 elem_type
 
-Sequence lengths are very flexible.  You can define the length as a constant, or define it as
-a Type (which will always be read from within the data), as the special values "sparse" or
-"nullterm", or leave it unspecified.
-If unspecified, any time the type is referenced in a type definition or data it will be
-followed by another opportunity to give this same length specifier.
+The type of all elements.  May be a Choice, including C<< scope->type_All >>.
 
-=over
+=head2 dimension_type
 
-=item len=CONSTANT
+Must be an integer type.  This will be used to encode the values of any dimension which was not
+specified as a constant.
 
-If set to a constant, the array has a fixed length and no further options are available.
+=head2 dimensions
 
-=item len=Type
-
-If set to a Type, a value of that type will be read from the data block on each occurrence
-of the array and used as the length,
-
-=item len="sparse"
-
-If set to the special value 'sparse', the elements of the sequence will each be prefixed with
-the index or identifier where they belong in the result.
-
-=item len="nullterm"
-
-If set to the special value 'nullterm', the array will be terminated by a type of zero or
-identifier of zero or value of zero, depending on other settings.
-
-=item len="any"
-
-In any reference within a meta block, you may specify the length as "any", and defer the length
-specification until later.  In the data block, this value may only be given for values of type
-"Type".
-
-=head2 len_const
-
-Holds the constant value of C<len> if C<len> is a constant.
-
-=head2 len_type
-
-Holds the type reference for C<len> if C<len> is a type.
-
-=head2 sparse
-
-True if C<len> is set to C<'sparse'>.
-
-=head2 nullterm
-
-True if C<len> is set to C<'nullterm'>.
-
-=head2 named_elems
-
-Specifies whether or not the elements have unique identifiers.  If true, the sequence behaves
-more like a record or map, and eny place where an element type is specified an element
-identifier will also be given.  If false the sequence is more like an array.
-
-=head2 elem_spec
-
-A single type (for all array elems), or arrayref of Type (and identifier if named_elems).
-If the array is longer than the list of types, additional elements will have their type (and
-identifier, if named_elems) specified inline.
+An ArrayRef which defines the dimensions of the array.  If this attribute itself is undefined,
+then the array dimensions will be encoded along with the data.  If the arrayref is defined,
+each undefined element represents a dimension that will be encoded along with the data.
 
 =cut
 
-has len         => ( is => 'ro', required => 1 );
-has named_elems => ( is => 'ro', required => 1 );
-has elem_spec   => ( is => 'ro', required => 1 );
-
-has len_const   => ( is => 'lazy' );
-has len_type    => ( is => 'lazy' );
-has sparse      => ( is => 'lazy' );
-has nullterm    => ( is => 'lazy' );
-sub _build_len_type  { $_[0]->len && ref($_[0]->len) && ref($_[0]->len)->can('isa_int')? $_[0]->len : undef }
-sub _build_len_const { defined $_[0]->len && !ref $_[0]->len && ($_[0]->len =~ /^[0-9]+$/)? $_[0]->len : undef }
-sub _build_sparse    { $_[0]->len eq 'sparse' }
-sub _build_nullterm  { $_[0]->len eq 'nullterm' }
+has elem_type       => ( is => 'ro', required => 1 );
+has dimension_count => ( is => 'ro', required => 1 );
+has dimension_type  => ( is => 'ro', required => 1 );
+has dimensions      => ( is => 'ro', required => 1 );
 
 sub isa_seq { 1 }
-
-sub has_scalar_component { !defined shift->len_const }
-
-has sizeof    => ( is => 'lazy', builder => sub { shift->_calc_size } );
-has bitsizeof => ( is => 'lazy', builder => sub { shift->sizeof << 3 } );
-
-sub _build_sizeof {
-	my ($self)= @_;
-	# Need constant length
-	my $len= $self->const_len;
-	return undef unless defined $len;
-	return 0 if !$len;
-	my $spec= $self->elem_spec;
-	if (ref $spec eq 'ARRAY') {
-		return undef unless @$spec >= $len;
-		my $bitsize= 0;
-		for (0 .. $len-1) {
-			my $type= $spec->[$_]{type};
-			# each element must also be constant-width
-			my $type_bits= $type->bitsizeof;
-			defined $type_bits or return undef;
-			# bit and byte alignment can be predicted; multibyte alignment can't
-			my $align= $spec->[$_]{align} || 0;
-			if ($align > 0) {
-				return undef; # can't predict length
-			} elsif ($align < 0) {
-				# This field is bit-aligned.
-				$bitsize += $type->bitsizeof;
-			}
-			else {
-				# Byte aligned.  If prev was bit-aligned, round up
-				$bitsize= ($bitsize + 7) & ~7 if $bitsize & 7;
-				$bitsize += $type->sizeof << 3;
-			}
-		}
-		return $bitsize;
-	}
-	else {
-		my $type= $spec->{type};
-		my $type_bits= $type->bitsizeof;
-		defined $type_bits or return undef;
-		my $align= $spec->{align} || 0;
-		if ($align > 0) {
-			return undef;
-		}
-		elsif ($align < 0) {
-			return (($type_bits * $len) + 7) & ~7;
-		}
-		else {
-			return (($type_bits + 7) & ~7) * $len;
-		}
-	}
-}
 
 1;
