9f4e9d33263fe6daa6cd1f91bde9b1d7bf20b8bf
Michael Conrad <mike@nrdvana.net>
2022-01-18 10:42:45 -0500

    Work in progress designing new decoder

--- c-userptiny/userptiny.c
+++ c-userptiny/userptiny.c
@@ -1,6 +1,14 @@
 #include "config.h"
 #include "userptiny.h"
 
+static inline bool userptiny_is_bzero(uint8_t *buf, size_t n) {
+	uint8_t *pos= buf + n;
+	while (pos != buf)
+		if (*--pos)
+			return false;
+	return true;
+}
+
 const char *userptiny_error_text(userptiny_error_t code) {
 	switch (code) {
 	case USERPTINY_EOVERFLOW:     return "integer overflow";
@@ -57,6 +65,8 @@ userptiny_scope_get_type(const struct userptiny_scope *scope, uint16_t type_id) 
 	}
 }
 
+/* Protocol Version 1 Default Scope */
+
 const char* const userptiny_v1_scope_symtable[]= {
 	"any",
 	"symref",
@@ -84,10 +94,14 @@ const struct userptiny_scope userptiny_v1_scope= {
 	.type_cache_count= sizeof(userptiny_v1_scope_types)/sizeof(*userptiny_v1_scope_types),
 };
 
+/* Decoder API */
+
 userptiny_error_t
-userptiny_dec_init(struct userptiny_dec *dec,
-                   const struct userptiny_scope *scope,
-                   uint8_t *state_storage, size_t state_storage_size
+userptiny_dec_init(
+	struct userptiny_dec *dec,
+	const struct userptiny_scope *scope,
+	uint8_t *state_storage,
+	size_t state_storage_size
 ) {
 	// allocate as many state structs as possible from the givenn bytes.  Needs to be
 	// correctly aligned, though.
@@ -106,36 +120,137 @@ userptiny_dec_init(struct userptiny_dec *dec,
 }
 
 userptiny_error_t
-userptiny_dec_set_input(struct userptiny_dec *dec,
-                        uint16_t root_type,
-                        uint8_t *in, uint16_t in_len
+userptiny_dec_reset(
+	struct userptiny_dec *dec,
+	uint16_t root_type,
+	uint8_t *in,
+	uint16_t in_len
 ) {
-	// look up root type
-	struct userptiny_type *type= userptiny_scope_get_type(dec->scope, root_type);
-	if (!type)
-		return USERPTINY_ETYPEREF;
 	// iterating bits, so need 3 spare bits in a uint16_t
 	if (in_len & 0xE000)
 		return USERPTINY_ELIMIT;
 	dec->input_end= in + in_len;
 	dec->bits_left= in_len << 3;
 	dec->state_pos= 0;
-	dec->state_stack[0].type= type;
-	dec->state_stack[0].elem_idx= 0;
-	return 0;
+	dec->node.type= root_type;
+	return userptiny_decode_node(dec, type);
 }
 
 userptiny_error_t
-userptiny_dec_int(struct userptiny_dec *dec, int16_t *out) {
-	struct userptiny_dec_state *st= &dec->state_stack[dec->state_pos];
-	switch(st->type->typeclass) {
-	case USERPTINY_TYPECLASS_INTEGER:
-		if (st->elem_idx)
-			return USERPTINY_EDOINGITWRONG;
-		st->elem_idx++;
-		return userptiny_decode_vint_s16(out, dec->input_end, &dec->bits_left);
+userptiny_decode_node(struct userptiny_dec *dec) {
+	userptiny_error_t err;
+	uint32_t u32;
+	int32 s32;
+	struct userptiny_type *type= userptiny_scope_get_type(dec->scope, dec->node.type);
+	if (!type) return USERPTINY_ETYPEREF;
+	dec->node.flags= 0;
+
+	switch(type->typeclass) {
+	/*
+	This implementation decodes all non-bigint integers as int32_t for the
+	user's convenience and code simplicity.  As such, these are the possible
+	scenarios:
+	1) A variable unsigned integer (which fits in 29 bits else is written as a BigInt)
+	2) A variable signed integer (in 28 bits, else is sign+magnitude BigInt)
+	3) A constant bit-size unsigned integer which must be <= 31 bits
+	4) A constant bit-size signed integer (in 2's complement) up to 32 bits,
+		and must be sign-extended if smaller than 32 bits.
+	
+	Any of these may be combined with a
+	1) scale (multiplier)
+	2) bias (offset)
+	*/
+	case USERPTINY_TYPECLASS_VINT_UNSIGNED:
+	case USERPTINY_TYPECLASS_VINT_SIGNED:
+		{ /* decode variable length int */
+			uint16_t bytes_left= dec->bits_left >> 3;
+			// need at least one byte
+			if (!bytes_left)
+				return USERPTINY_EOVERRUN;
+			// This byte is either a small positive int, or determines how mmuch larger the int is
+			uint8_t sel= dec->input_end[-bytes_left];
+			// Bit 0 indicates something longer than 1 byte
+			if (!(sel & 1)) {
+				dec->node.as_int32= sel >> 1;
+				dec->node.flags= USERPTINY_NODEFLAG_INT32;
+				bytes_left -= 1;
+			}
+			else if (!(sel & 2)) {
+				int sz= 2 + ((sel >> 2) & 1);
+				// read a uint16, or uint32?
+				if (bytes_left < sz)
+					return USERPTINY_EOVERRUN;
+				if (type->typeclass == USERPTINY_TYPECLASS_VINT_SIGNED) {
+					dec->node.as_int32= sz == 2
+						? ((int16_t)userptiny_load_le16(dec->input_end - bytes_left) >> 3)
+						: ((int32_t)userptiny_load_le32(dec->input_end - bytes_left) >> 3);
+				} else {
+					dec->node.as_int32= sz == 2
+						? (int32_t)(userptiny_load_le16(dec->input_end - bytes_left) >> 3)
+						: (int32_t)(userptiny_load_le32(dec->input_end - bytes_left) >> 3);
+				}
+				dec->node.flags= USERPTINY_NODEFLAG_INT32;
+				bytes_left -= sz;
+			}
+			// Else enter BigInt territory.  It's some number of uint32 limbs with a separate sign bit.
+			else {
+				bool negative= (type->typeclass == USERPTINY_TYPECLASS_VINT_UNSIGNED)? 0
+					: (sel >> 2) & 1;
+				uint16_t limbs= sel >> 3;
+				// 0 means read the size from a word double the size
+				if (!limbs) {
+					if (bytes_left < 2)
+						return USERPTINY_EOVERRUN;
+					limbs= userptiny_load_le16(dec->input_end - bytes_left);
+					bytes_left -= 2;
+					// don't bother reading 32-bit word for the size, just give up
+					if (!limbs)
+						return USERPTINY_EUNSUPPORTED;
+					if (limbs > 0x1FFF)
+						return USERPTINY_EOVERFLOW;
+				}
+				if (bytes_left < (limbs << 2))
+					return USERPTINY_EOVERFLOW;
+				// The BigInt could possibly fit in 31bit, making the caller's life easier.
+				// The sign bit still comes from the selector though, not twos complement.
+				if ((limbs == 1 || is_bzero(dec->input_end - bytes_left + 4, (limbs-1)<<2))
+					&& (s32= (int32_t)userptiny_load_le32(dec->input_end - bytes_left)) >= 0
+				) {
+					dec->node.as_int32= negative? -s32 : s32;
+					dec->node.flags= USERPTINY_NODEFLAG_INT32;
+				}
+				else {
+					dec->node.as_bigint.is_negative= negative;
+					dec->node.as_bigint.limb_count= limbs;
+					dec->node.as_bigint.limbs= dec->input_end - bytes_left;
+					dec->node.flags= USERPTINY_NODEFLAG_BIGINT;
+				}
+				bytes_left -= limbs << 2;
+			}
+			*bits_left= bytes_left << 3;
+		}
+		if (0) {
+	case USERPTINY_TYPECLASS_INT_BITS:
+			err= userptiny_decode_bits_u32(&u32, dec->input_end, &dec->bits_left, type->integer.bits);
+			if (err) return err;
+			dec->node.as_int32= u32;
+			dec->node.flags= USERPTINY_NODEFLAG_INT32;
+		}
+		if (0) {
+	case USERPTINY_TYPECLASS_INT_TWOS:
+			err= userptiny_decode_bits_s32(&s32, dec->input_end, &dec->bits_left, type->integer.bits);
+			if (err) return err;
+			dec->node.as_int32= u32;
+			dec->node.flags= USERPTINY_NODEFLAG_INT32;
+		}
+		// TODO: scale and offset
+		if (type->flags & (USERPTINY_TYPEFLAG_INT_BASE|USERPTINY_TYPEFLAG_INT_SCALE)) {
+			return USERPTINY_EUNSUPPORTED;
+		}
+		break;
+	}
 	default:
-		return USERPTINY_EDOINGITWRONG;
+		return USERPTINY_EUNSUPPORTED;
 	}
 }
 
@@ -174,6 +289,38 @@ userptiny_decode_bits_s16(int16_t *out, uint8_t *buf_lim, uint16_t *bits_left, u
 }
 
 userptiny_error_t
+userptiny_skip_vint(size_t how_many, uint8_t *buf_lim, uint16_t *bits_left) {
+	// switch to bytes for the moment
+	uint16_t bytes_left= *bits_left >> 3;
+	while (how_many--) {
+		// need at least one byte
+		if (!bytes_left)
+			return USERPTINY_EOVERRUN;
+		// This byte determines how many more will be read
+		uint8_t sel= buf_lim[-bytes_left--];
+		if (sel & 1) {
+			int16_t need_bytes= !(sel & 2)? 1
+				: !(sel & 4)? 3
+				: (int16_t)(int8_t)(sel & 0xF8);
+			if (!need_bytes) {
+				if (bytes_left < 2)
+					return USERPTINY_EOVERRUN;
+				need_bytes= (int16_t) userptiny_load_le16(buf_lim-bytes_left);
+				bytes_left -= 2;
+				if (!need_bytes)
+					return USERPTINY_EOVERRUN;
+				need_bytes &= 0xFFF8;
+			}
+			if (bytes_left < need_bytes)
+				return USERPTINY_EOVERRUN;
+			bytes_left -= need_bytes;
+		}
+	}
+	*bits_left= bytes_left << 3;
+	return 0;
+}
+
+userptiny_error_t
 userptiny_decode_vint_u16(uint16_t *out, uint8_t *buf_lim, uint16_t *bits_left) {
 	// switch to bytes for the moment
 	uint16_t bytes_left= *bits_left >> 3;
@@ -206,67 +353,43 @@ userptiny_decode_vint_u16(uint16_t *out, uint8_t *buf_lim, uint16_t *bits_left) 
 	return 0;
 }
 
-userptiny_error_t
-userptiny_decode_vint_s16(int16_t *out, uint8_t *buf_lim, uint16_t *bits_left) {
-	// switch to bytes for the moment
-	uint16_t bytes_left= *bits_left >> 3;
-	// need at least one byte
-	if (!bytes_left)
-		return USERPTINY_EOVERRUN;
-	// This byte determines how many more will be read
-	uint8_t sel= buf_lim[-bytes_left--];
-	if (!(sel & 1)) {
-		*out= (int8_t)sel >> 1;
-	}
-	else if (!(sel & 2)) {
-		if (!bytes_left)
-			return USERPTINY_EOVERRUN;
-		*out= (sel >> 2) | ((int16_t)(int8_t)buf_lim[-bytes_left--] << 6);
-	}
-	else if (!(sel & 4)) {
-		if (bytes_left < 3)
-			return USERPTINY_EOVERRUN;
-		if ((buf_lim[-bytes_left+1] >> 3) || buf_lim[-bytes_left+2])
-			return USERPTINY_EOVERFLOW;
-		*out= (sel >> 3) | ((uint16_t)buf_lim[-bytes_left] << 5) | ((int16_t)(int8_t)buf_lim[-bytes_left+1] << 13);
-		bytes_left -= 3;
-	}
-	else {
-		// just assume value in bigint is larger than int16
-		return USERPTINY_EOVERFLOW;
-	}
-	*bits_left= bytes_left << 3;
-	return 0;
-}
+/*  Alternate option for variable-length integers using a simple loop
 
 userptiny_error_t
-userptiny_skip_vint(size_t how_many, uint8_t *buf_lim, uint16_t *bits_left) {
+userptiny_decode_vint {
 	// switch to bytes for the moment
 	uint16_t bytes_left= *bits_left >> 3;
-	while (how_many--) {
+	uint32_t accum= 0;
+	unsigned shift= 0;
+	unsigned byte;
+	while (1) {
 		// need at least one byte
 		if (!bytes_left)
 			return USERPTINY_EOVERRUN;
-		// This byte determines how many more will be read
-		uint8_t sel= buf_lim[-bytes_left--];
-		if (sel & 1) {
-			uint16_t need_bytes= !(sel & 2)? 1
-				: !(sel & 4)? 3
-				: (uint16_t)(sel & 0xF8) + 8 + ((intptr_t)&buf_lim[-bytes_left] & 7);
-			if (need_bytes > 0xFF) {
-				if (bytes_left < 2)
-					return USERPTINY_EOVERRUN;
-				need_bytes= buf_lim[-bytes_left] | ((uint16_t)buf_lim[-bytes_left+1] << 8);
-				bytes_left -= 2;
-				if (need_bytes > 0x1FFE)
-					return USERPTINY_EOVERRUN;
-				need_bytes= (need_bytes << 3) + 8 + ((intptr_t)&buf_lim[-bytes_left] & 7);
-			}
-			if (bytes_left < need_bytes)
-				return USERPTINY_EOVERRUN;
-			bytes_left -= need_bytes;
-		}
+		byte= buf_lim[-bytes_left--];
+		// last byte?
+		if (byte & 1)
+			break;
+		accum |= (uint32_t)(byte >> 1) << shift;
+		shift += 7;
+		if (shift > 32)
+			return USERP_EOVERFLOW;
 	}
-	*bits_left= bytes_left << 3;
-	return 0;
+	// next bit is 'bigint' flag
+	is_bigint= (byte & 2);
+	// If signed, next bit is sign
+	if (is_signed) {
+		is_negative= (byte & 4);
+		byte >>= 3;
+	} else {
+		byte >>= 2;
+	}
+	if (shift > 24 && (byte >> (32 - shift)))
+		return USERPTINY_EOVERFLOW;
+	accum |= (uint32_t)byte << shift;
+	if (is_bigint) {
+		if (bytes_left < accum)
+			return USERPTINY_EOVERFLOW;
+	}
 }
+*/
--- c-userptiny/userptiny.h
+++ c-userptiny/userptiny.h
@@ -1,6 +1,20 @@
 #ifndef USERPTINY_H
 #define USERPTINY_H
 
+/*
+Error codes:
+
+  EOVERFLOW     - The data contains a value that exceeds the limits of this implementation.
+  EOVERRUN      - The data is invalid, describing a structure that exceeds the end of the buffer.
+  EUNSUPPORTED  - The data uses a feature not implemented by this code.
+  ELIMIT        - The data exceeds a constraint requested by the user of the library.
+  ETYPEREF      - The data makes an invalid reference to a Userp Type
+  ESYMREF       - The data makes an invalid reference to a Userp Symbol
+  EALLOC        - The library does not have enough temporary storage to perform an operation
+  EDOINGITWRONG - Your program made an invalid request to the library
+
+*/
+
 typedef uint8_t userptiny_error_t;
 
 #define USERPTINY_EOVERFLOW     1
@@ -12,13 +26,27 @@ typedef uint8_t userptiny_error_t;
 #define USERPTINY_EALLOC        7
 #define USERPTINY_EDOINGITWRONG 8
 
+/* Return a short constant string describing the error code */
 const char *userptiny_error_text(userptiny_error_t code);
+
+/* Userp Type Structure
+
+This struct describes a userp type.  It contains just enough fields for the
+implementation to be able to decode the supported features of each type.
+All type-specific fields are stored in a union, and you must inspect the
+'typeclass' and 'flags' before using them.
+
+*/
 
 #define USERPTINY_TYPECLASS_BUILTIN  1
 #define USERPTINY_TYPECLASS_INTEGER  2
 #define USERPTINY_TYPECLASS_CHOICE   3
 #define USERPTINY_TYPECLASS_ARRAY    4
 #define USERPTINY_TYPECLASS_RECORD   5
+
+#define USERPTINY_TYPEFLAG_INT_SIGNED 1
+#define USERPTINY_TYPEFLAG_INT_BASE   2
+#define USERPTINY_TYPEFLAG_INT_SCALE  4
 
 struct userptiny_type {
 	uint16_t
@@ -30,7 +58,8 @@ struct userptiny_type {
 			uint8_t subtype;
 		} builtin;
 		struct userptiny_type_int {
-			uint16_t min, max;
+			uint16_t base;
+			uint8_t scale;
 			uint8_t bits;
 		} integer;
 		struct userptiny_type_array {
@@ -48,10 +77,16 @@ struct userptiny_type {
 	};
 };
 
-#define USERPTINY_V1_TYPE_ANY     1
-#define USERPTINY_V1_TYPE_SYMREF  2
-#define USERPTINY_V1_TYPE_TYPEREF 3
-#define USERPTINY_V1_TYPE_INTEGER 4
+/* Userp Scope structure
+
+A scope defines a collection of symbols and types, and allows multiple blocks
+to share the same type definitions.  This "tiny" implementation requires that
+the buffer the scope was decoded from continue to exist for the lifespan of
+the Scope.  It also cannot necessarily inflate all types or symbols into
+their own structs, and may need to seek around re-decoding the original data
+depending on which types are being decoded from the current block.
+
+*/
 
 struct userptiny_scope {
 	const struct userptiny_scope
@@ -72,19 +107,53 @@ struct userptiny_scope {
 		type_cache_count;
 };
 
+/* Initialize a scope by parsing a symbol and type table */
 userptiny_error_t
 userptiny_scope_init(struct userptiny_scope *scope,
                      const struct userptiny_scope *parent,
-                     uint8_t *state_storage, size_t state_storage_size);
+                     uint8_t *state_storage, size_t state_storage_size,
+                     uint8_t *data, uint16_t data_size);
 
-userptiny_error_t
-userptiny_scope_parse(struct userptiny_scope *scope,
-                      uint8_t *data, uint16_t data_size);
+/* Find a numbered symbol and return a pointer to its string */
+const char *
+userptiny_scope_get_symbol(const struct userptiny_scope *scope, uint16_t symbol);
 
+/* Find a numbered type and return a pointer to its struct.
+The struct is only valid until the next call to userptiny_get_type,
+unless the scope and all its parents are fully inflated.
+*/
 struct userptiny_type *
 userptiny_scope_get_type(const struct userptiny_scope *scope, uint16_t type_id);
 
+/* Userp Version-1 standard scope
+
+Version 1 of the protocol contains a collection of common types that are
+defined in the default scope.  Scopes do not have to inherit from the default
+scope, but it is very convenient to do so, and the default scope can be fully
+stored in static memory.
+
+  - ANY
+    The conceptual choice of all other defined types
+  - SYMBOL
+    A reference to a symbolic constant, previously declared in the symbol table
+  - TYPEREF
+    A reference to a type, previously declared in the type table
+  - INTEGER
+    The infinite collection of negative and positive integers, stored as a
+    variable-length format that extends into a BigInt pattern.
+
+*/
+
+#define USERPTINY_V1_TYPE_ANY     1
+#define USERPTINY_V1_TYPE_SYMBOL  2
+#define USERPTINY_V1_TYPE_TYPEREF 3
+#define USERPTINY_V1_TYPE_INTEGER 4
+
 extern const struct userptiny_scope userptiny_v1_scope;
+
+/* Userp encoder
+
+*/
 
 struct userptiny_enc {
 	const struct userptiny_scope *scope;
@@ -97,6 +166,18 @@ struct userptiny_enc {
 		error;
 	uint8_t state_stack[];
 };
+
+/* Userp Node Info
+
+This struct describes the value of the "current" point within the data block.
+The decoder functions all unpack to this same struct, and then the user of the
+library can inspect the node without knowing in advance what type was about to
+be decoded.
+
+The struct is heavily "unioned" and fields should only be used after inspecting
+the flags.
+
+*/
 
 #define USERPTINY_NODEFLAG_INT      0x0001
 #define USERPTINY_NODEFLAG_SIGNED   0x0002
@@ -111,11 +192,11 @@ struct userptiny_node_info {
 	union {
 		struct {
 			union {
-				uint32_t as_uint32;
 				int32_t  as_int32;
 				struct {
 					uint64_t *limbs;
-					uint16_t limb_count;
+					uint16_t limb_count:16;
+					bool is_negative;
 				} as_bigint;
 			};
 			uint16_t as_symbol;
@@ -139,6 +220,15 @@ struct userptiny_node_info {
 	uint16_t type;
 };
 
+/* Userp Decoder
+
+The decoder operates on one block at a time, stepping or seeking through the
+elements of the data and loading each into the 'node' field.  In order to
+descend into records and arrays, it needs to have room on the "state stack",
+which must be provided by the caller during initialization.
+
+*/
+
 struct userptiny_dec_state {
 	struct userptiny_type
 		*type;
@@ -159,18 +249,27 @@ struct userptiny_dec {
 		*state_stack;
 };
 
+/* Initialize a decoder struct, and provide the state storage needed by the app */
 userptiny_error_t
 userptiny_dec_init(struct userptiny_dec *dec,
                    const struct userptiny_scope *scope,
                    uint8_t *state_storage, size_t state_storage_size);
 
+/* Begin decoding a block, with the root node loaded into dec->node_info */
 userptiny_error_t userptiny_dec_reset(struct userptiny_dec *dec, uint16_t root_type, uint8_t *in, uint16_t in_len);
-userptiny_error_t userptiny_dec_begin(struct userptiny_dec *dec);
-userptiny_error_t userptiny_dec_seek_elem(struct userptiny_dec *dec, uint16_t idx);
-userptiny_error_t userptiny_dec_seek_field_idx(struct userptiny_dec *dec, uint16_t idx);
-userptiny_error_t userptiny_dec_seek_field_sym(struct userptiny_dec *dec, uint16_t sym_id);
+
+/* Descend into an array or record node, to iterate its contents. */
+userptiny_error_t userptiny_dec_enter(struct userptiny_dec *dec);
+/* Return to a parent array or record, pointing at the same node as before the 'enter' call. */
+userptiny_error_t userptiny_dec_exit(struct userptiny_dec *dec);
+/* Read the next element in the sequence, or arrive at a final state for the current iteration. */
 userptiny_error_t userptiny_dec_next(struct userptiny_dec *dec);
-userptiny_error_t userptiny_dec_end(struct userptiny_dec *dec);
+/* Seek to an element of the currently iterating array */
+userptiny_error_t userptiny_dec_seek_elem(struct userptiny_dec *dec, uint16_t idx);
+/* Seek to a field of the currently iterating record, by field index according to the type definition */
+userptiny_error_t userptiny_dec_seek_field_idx(struct userptiny_dec *dec, uint16_t idx);
+/* Seek to a field of the currently itreating record, by field name (symbol ID) */
+userptiny_error_t userptiny_dec_seek_field_sym(struct userptiny_dec *dec, uint16_t sym_id);
 
 userptiny_error_t userptiny_decode_bits_u16(uint16_t *out, uint8_t *buf_lim, uint16_t *bits_left, uint8_t bits);
 userptiny_error_t userptiny_decode_bits_s16( int16_t *out, uint8_t *buf_lim, uint16_t *bits_left, uint8_t bits);
